7f5a36cb5041cc43822b166a15249e01e5460f9b
Thomas Ng <thomas.ng@nikhef.nl>
2025-12-12 09:16:25 +0100

    Update propagate_name method to accept Sequence[str] instead of tuple[str, ...]

--- src/jimgw/core/transforms.py
+++ src/jimgw/core/transforms.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Callable
+from typing import Callable, Sequence
 
 import jax
 import jax.numpy as jnp
@@ -22,7 +22,7 @@ class Transform(ABC):
     ):
         self.name_mapping = name_mapping
 
-    def propagate_name(self, x: tuple[str, ...]) -> tuple[str, ...]:
+    def propagate_name(self, x: Sequence[str]) -> tuple[str, ...]:
         # Remove names in from_list, then append names in to_list (preserving order)
         result = [name for name in x if name not in self.name_mapping[0]]
         result += [name for name in self.name_mapping[1] if name not in result]
