a2f466165a623289cc184c8828be1aa8b277e0bb
Thomas Ng <thomas.ng@nikhef.nl>
2026-01-19 13:46:06 +0100

    Refactor SpinAnglesToCartesianSpinTransform to define inverse transform function with phase handling

--- src/jimgw/core/single_event/transforms.py
+++ src/jimgw/core/single_event/transforms.py
@@ -84,6 +84,39 @@ class SpinAnglesToCartesianSpinTransform(ConditionalBijectiveTransform):
                     "s2_y": s2y,
                     "s2_z": s2z,
                 }
+
+            def named_inverse_transform(x):
+                (
+                    theta_jn,
+                    phi_jl,
+                    tilt_1,
+                    tilt_2,
+                    phi_12,
+                    a_1,
+                    a_2,
+                ) = cartesian_spin_to_spin_angles(
+                    x["iota"],
+                    x["s1_x"],
+                    x["s1_y"],
+                    x["s1_z"],
+                    x["s2_x"],
+                    x["s2_y"],
+                    x["s2_z"],
+                    x["M_c"],
+                    x["q"],
+                    self.freq_ref,
+                    0.0,
+                )
+
+                return {
+                    "theta_jn": theta_jn,
+                    "phi_jl": phi_jl,
+                    "tilt_1": tilt_1,
+                    "tilt_2": tilt_2,
+                    "phi_12": phi_12,
+                    "a_1": a_1,
+                    "a_2": a_2,
+                }
         else:
 
             def named_transform(x):
@@ -110,38 +143,38 @@ class SpinAnglesToCartesianSpinTransform(ConditionalBijectiveTransform):
                     "s2_z": s2z,
                 }
 
-        def named_inverse_transform(x):
-            (
-                theta_jn,
-                phi_jl,
-                tilt_1,
-                tilt_2,
-                phi_12,
-                a_1,
-                a_2,
-            ) = cartesian_spin_to_spin_angles(
-                x["iota"],
-                x["s1_x"],
-                x["s1_y"],
-                x["s1_z"],
-                x["s2_x"],
-                x["s2_y"],
-                x["s2_z"],
-                x["M_c"],
-                x["q"],
-                self.freq_ref,
-                x["phase_c"],
-            )
+            def named_inverse_transform(x):
+                (
+                    theta_jn,
+                    phi_jl,
+                    tilt_1,
+                    tilt_2,
+                    phi_12,
+                    a_1,
+                    a_2,
+                ) = cartesian_spin_to_spin_angles(
+                    x["iota"],
+                    x["s1_x"],
+                    x["s1_y"],
+                    x["s1_z"],
+                    x["s2_x"],
+                    x["s2_y"],
+                    x["s2_z"],
+                    x["M_c"],
+                    x["q"],
+                    self.freq_ref,
+                    x["phase_c"],
+                )
 
-            return {
-                "theta_jn": theta_jn,
-                "phi_jl": phi_jl,
-                "tilt_1": tilt_1,
-                "tilt_2": tilt_2,
-                "phi_12": phi_12,
-                "a_1": a_1,
-                "a_2": a_2,
-            }
+                return {
+                    "theta_jn": theta_jn,
+                    "phi_jl": phi_jl,
+                    "tilt_1": tilt_1,
+                    "tilt_2": tilt_2,
+                    "phi_12": phi_12,
+                    "a_1": a_1,
+                    "a_2": a_2,
+                }
 
         self.transform_func = named_transform
         self.inverse_transform_func = named_inverse_transform
