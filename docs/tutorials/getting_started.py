e29d122a55453bbe2cab8e4612fcfc23270f05ae
Thomas Ng <thomas.ng@nikhef.nl>
2026-03-25 15:07:55 +0100

    fix: update getting started tutorial

--- docs/tutorials/getting_started.py
+++ docs/tutorials/getting_started.py
@@ -15,8 +15,8 @@
 #
 # This tutorial walks through a complete Jim run on a toy problem.
 # We inject a non-spinning BNS signal using TaylorF2, fix every
-# parameter except chirp mass ($\mathcal{M}_c$) and luminosity distance
-# ($d_L$), and sample just those two to keep things fast and easy to visualise.
+# parameter except chirp mass ($\mathcal{M}_c$) and symmetric mass ratio
+# ($\eta$), and sample just those two to keep things fast and easy to visualise.
 
 # %% [markdown]
 # ## Setup
@@ -24,10 +24,7 @@
 # %%
 import jax
 
-jax.config.update("jax_enable_x64", True)
-# jax.config.update("jax_disable_jit", True)
-
-from jimgw.core.single_event.detector import get_H1, get_L1
+from jimgw.core.single_event.detector import get_H1
 from jimgw.core.single_event.waveform import RippleTaylorF2
 from jimgw.core.single_event.likelihood import TransientLikelihoodFD
 from jimgw.core.single_event.gps_times import (
@@ -36,11 +33,13 @@ from jimgw.core.single_event.gps_times import (
 from jimgw.core.prior import CombinePrior, UniformPrior
 from jimgw.core.jim import Jim
 
+jax.config.update("jax_enable_x64", True)
+
 # %% [markdown]
 # ## 1. Define injection parameters
 #
 # We use a non-spinning BNS signal with zero tidal deformability. Only
-# `M_c` and `d_L` will be sampled; every other parameter is fixed to
+# `M_c` and `eta` will be sampled; every other parameter is fixed to
 # the injection value.
 
 # %%
@@ -48,13 +47,13 @@ gps_time = 1126259462.0
 gmst = compute_gmst(gps_time)
 
 injection_parameters = {
-    "M_c": 1.2,
+    "M_c": 30.0,
     "eta": 0.24,
     "s1_z": 0.0,
     "s2_z": 0.0,
-    "lambda_1": 0.0,  # tidal deformability fixed to zero
+    "lambda_1": 0.0,
     "lambda_2": 0.0,
-    "d_L": 40.0,
+    "d_L": 500.0,
     "t_c": 0.0,
     "phase_c": 0.0,
     "iota": 0.4,
@@ -72,11 +71,11 @@ injection_parameters = {
 waveform = RippleTaylorF2(f_ref=20.0)
 
 f_min = 20.0
-f_max = 1024.0
+f_max = 512.0
 duration = 4.0
 sampling_frequency = 2 * f_max
 
-ifos = [get_H1(), get_L1()]
+ifos = [get_H1()]
 for ifo in ifos:
     ifo.load_and_set_psd()
     ifo.frequency_bounds = (f_min, f_max)
@@ -92,12 +91,12 @@ for ifo in ifos:
 # %% [markdown]
 # ## 3. Define the prior
 #
-# We only sample `M_c` and `d_L`, so we only need priors for those two.
+# We only sample `M_c` and `eta`, so we only need priors for those two.
 
 # %%
 prior = CombinePrior([
-    UniformPrior(0.9, 1.5, ["M_c"]),
-    UniformPrior(10.0, 200.0, ["d_L"]),
+    UniformPrior(29.0, 31.0, ["M_c"]),
+    UniformPrior(0.2, 0.25, ["eta"])
 ])
 
 # %% [markdown]
@@ -110,7 +109,7 @@ prior = CombinePrior([
 fixed_parameters = {
     k: v
     for k, v in injection_parameters.items()
-    if k not in ["M_c", "d_L", "trigger_time", "gmst"]
+    if k not in ["M_c", "eta", "trigger_time", "gmst"]
 }
 
 likelihood = TransientLikelihoodFD(
@@ -131,13 +130,18 @@ likelihood = TransientLikelihoodFD(
 jim = Jim(
     likelihood,
     prior,
-    n_chains=200,
+    n_chains=100,
     n_local_steps=50,
-    n_global_steps=200,
-    n_training_loops=1,
-    n_production_loops=5,
+    n_global_steps=100,
+    n_training_loops=5,
+    n_production_loops=3,
+    n_epochs=5,
+    rq_spline_hidden_units=[32, 32],
+    rq_spline_n_layers=4,
+    rq_spline_n_bins=8,
+    n_max_examples=3000,
+    n_temperatures=0,
     verbose=True,
-    n_temperatures=0,  # no parallel tempering for this simple example
 )
 
 jim.sample()
@@ -146,4 +150,33 @@ jim.sample()
 # ## 6. Inspect the results
 
 # %%
+import corner
+import matplotlib.pyplot as plt
+import numpy as np
+
 samples = jim.get_samples()
+
+# %% [markdown]
+# ### Corner plot
+#
+# The corner plot shows the 1-D and 2-D marginal posteriors for the two
+# sampled parameters. The dashed lines mark the injected (true) values.
+
+# %%
+param_labels = [r"$\mathcal{M}_c\ [M_\odot]$", r"$\eta$"]
+truths = [injection_parameters["M_c"], injection_parameters["eta"]]
+
+data = np.column_stack([
+    np.asarray(samples["M_c"]).flatten(),
+    np.asarray(samples["eta"]).flatten(),
+])
+
+fig = corner.corner(
+    data,
+    labels=param_labels,
+    truths=truths,
+    truth_color="tab:orange",
+    show_titles=True,
+    title_kwargs={"fontsize": 12},
+)
+plt.show()
